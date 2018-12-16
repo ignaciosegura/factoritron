5b070dc938ab17d1bb78f54e415d41c421bbc389
Ignacio Segura <nacho@isegura.es>
2018-12-16 23:03:19 +0100

    Updates

---
+++ precache-manifest.c0936360f62619ba8e7c566ef925988f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d84ecb71a97c94b6ca5c",
+    "url": "/factoritron/static/css/main.f2680339.chunk.css"
+  },
+  {
+    "revision": "d84ecb71a97c94b6ca5c",
+    "url": "/factoritron/static/js/main.d84ecb71.chunk.js"
+  },
+  {
+    "revision": "d4965d296f78695ffbe6",
+    "url": "/factoritron/static/js/1.d4965d29.chunk.js"
+  },
+  {
+    "revision": "b73d292e4f1f0b9658c0",
+    "url": "/factoritron/static/js/runtime~main.b73d292e.js"
+  },
+  {
+    "revision": "05ab7bc44cb07517fa18b3d9aa77462e",
+    "url": "/factoritron/index.html"
+  }
+];
